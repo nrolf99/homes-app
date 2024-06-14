c09e32af8c00de3e072917a195ef009e0c57c3f3
nrolf99 <nrolf99@gmail.com>
2024-06-14 16:18:40 -0500

    mid fic, figure out why pic wont load in housing-location component view

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { HousingLocationComponent } from '../housing-location/housing-location.component';
-
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HousingLocationComponent} from '../housing-location/housing-location.component';
+import {HousingLocation} from '../housinglocation';
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
       <form>
@@ -13,15 +14,21 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
       </form>
     </section>
     <section class="results">
-      <app-housing-location></app-housing-location>
-    </section>
+    <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+  </section>
   `,
   styleUrls: ['./home.component.css'],
-  imports: [
-    CommonModule, 
-    HousingLocationComponent
-  ]
 })
 export class HomeComponent {
-
+  readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   templateUrl: './housing-location.component.html',
   styleUrls: ['./housing-location.component.css'], 
-  standalone: true
+  standalone: true,
+  imports: [CommonModule]
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
