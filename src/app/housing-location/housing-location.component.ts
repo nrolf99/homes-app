139214b55b53a63ea734c66945dfda06a7491795
nrolf99 <nrolf99@gmail.com>
2024-06-15 15:43:07 -0500

    showing housing location components on home component

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -4,7 +4,18 @@ import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
-  templateUrl: './housing-location.component.html',
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+    </section>
+  `,  
   styleUrls: ['./housing-location.component.css'], 
   standalone: true,
   imports: [CommonModule]
